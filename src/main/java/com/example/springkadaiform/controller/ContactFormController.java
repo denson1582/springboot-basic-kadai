bd32f283f3ae1b992d53a5efbdc3ae0f03bd6f91
denson <denson034@gmail.com>
2025-08-13 17:24:33 +0900

    変更

--- src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.springkadaiform.form.ContactForm;
 
@@ -14,27 +15,29 @@ import com.example.springkadaiform.form.ContactForm;
 public class ContactFormController {
 	@GetMapping("/form")
 	public String form(Model model) {
-		ContactForm contactForm = new ContactForm();
-		model.addAttribute("contactForm", contactForm);
+		// フラッシュ属性から値があればセット
+		if (!model.containsAttribute("contactForm")) {
+			ContactForm contactForm = new ContactForm();
+			model.addAttribute("contactForm", contactForm);
+		}
 		return "contactFormView";
 	}
 	
 	@PostMapping("/confirm")
     public String confirm(@Validated @ModelAttribute ContactForm contactForm,
                           BindingResult bindingResult,
-                          Model model) {
+                          Model model,
+                          RedirectAttributes redirectAttributes) {
 
         // バリデーションエラーがある場合
         if (bindingResult.hasErrors()) {
-            // エラー情報をモデルに追加し、元のフォームページに戻る
-            // contactForm (エラー情報を含む) は自動的にモデルに追加されている
-            return "contactFormView";
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.contactForm", bindingResult);
+            redirectAttributes.addFlashAttribute("contactForm", contactForm);
+            return "redirect:/form";
         }
 
         // バリデーションOKの場合、確認画面を表示
         // contactFormは自動的にModelに追加されており、confirmViewで参照可能
         return "confirmView"; // confirmView.html を表示
     }
-	
-	
 }
